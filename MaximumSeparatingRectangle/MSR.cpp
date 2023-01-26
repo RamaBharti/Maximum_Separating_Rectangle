a30d55a08b634af7a945bbaf3fbd25281b8d2d8c
Rama Bharti <77854653+RamaBharti@users.noreply.github.com>
2023-01-26 16:32:24 +0530

    Update MSR.cpp

--- MaximumSeparatingRectangle/MSR.cpp
+++ MaximumSeparatingRectangle/MSR.cpp
@@ -1,4 +1,4 @@
-//
+//MSR
 //It is a variation of the Maximum Inner Rectangle problem and is NP-hard. There is no known polynomial-time algorithm for solving the problem, but there are several approximate and heuristic approaches that have been proposed, including the use of linear programming, genetic algorithms, and branch-and-bound techniques.
 //The staircase approach
 //The staircase approach is a method that can be used to find the maximum area rectangle that separates a set of red and blue points. 
